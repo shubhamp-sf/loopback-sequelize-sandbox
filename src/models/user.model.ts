dff523f9efc7a1e096720f7f40cc3ce72939e351
Shubham Prajapat <shubham.prajapat@sourcefuse.com>
2022-09-29 11:56:50 +0530

    add artifacts to for `/users`

---
+++ src/models/user.model.ts
@@ -0,0 +1,38 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+  })
+  name?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'number',
+  })
+  age?: number;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
